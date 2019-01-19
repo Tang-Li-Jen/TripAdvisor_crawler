69bb2130c11659881931b368c150fa0dae50d1b3
Li-Jen Tang <b00701160@gmail.com>
2019-01-20 04:00:04 +0800

    refine code

--- content_parser.py
+++ content_parser.py
@@ -7,79 +7,69 @@ import requests
 import time
 
 domain = 'https://www.tripadvisor.com.tw'
-
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
 
-hotel_name = []
-hotel_url = []
-n_comment = []
-address =[]
-phone = []
-rank = []
-comment_star = []
-p_Excellent = []
-p_VeryGood = []
-p_Average = []
-p_Poor = []
-p_Terrible = []
-price_range = []
-hotel_star = []
+df = pd.read_csv('./data/url_parser.csv')
+total_hotels = len(df)
+with open('./data/content_parser.csv', 'a') as csvfile:
+    fieldnames = [
+                    'hotel_id', 'address', 'rank', 
+                    'phone','p_Excellent', 'p_VeryGood':, 'p_Average',
+                    'p_Poor', 'p_Terrible'
+                 ]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
 
-df = pd.read_csv('url_parser.csv')
-total_len = len(df)
-for index, u in enumerate(df.url[:10]):
-    print 'process = {}/{}'.format(index+1, total_len)
-    hotel_url.append(u)
-    r = requests.get(u, headers=headers)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    for i, u in df[['hotel_id','url']]:
+        print('process = {}/{}'.format(i+1, total_hotels))
+        r = requests.get(u, headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        # info_block
+        info_block = soup.find('div', {'id':"atf_header"})
+        address = info_block.find('span', {'class':"detail"}).text
+        try:
+            phone = info_block.find('span', {'class':"is-hidden-mobile detail"}).text
+        except AttributeError:
+            phone = None
+        rank = info_block.find('span', {'class':"header_popularity popIndexValidation"}).text
+        # comment_star.append(soup.find('span', {'class':'hotels-hotel-review-about-with-photos-Reviews__overallRating--3cjYf'}).text)
 
-    hotel_name.append(soup.find('h1',{'class':'ui_header h1'}).text)
-    # info_block
-    info_block = soup.find('div', {'id':"atf_header"})
-    address.append(info_block.find('span', {'class':"detail"}).text)
-    try:
-        phone.append(info_block.find('span', {'class':"is-hidden-mobile detail"}).text)
-    except AttributeError:
-        phone.append(None)
-    n_comment.append(info_block.find('span', {'class':"reviewCount"}).text)
-    rank.append(info_block.find('span', {'class':"header_popularity popIndexValidation"}).text)
-    # comment_star.append(soup.find('span', {'class':'hotels-hotel-review-about-with-photos-Reviews__overallRating--3cjYf'}).text)
+        # about_block
+        about_block = soup.find('div', {'class':"hotels-hotel-review-layout-Section__plain--1HV0a"})
+        # price_range.append(about_block.find('div', {'class':'hotels-hotel-review-about-addendum-AddendumItem__content--28NoV'}).text)
 
-    # about_block
-    about_block = soup.find('div', {'class':"hotels-hotel-review-layout-Section__plain--1HV0a"})
-    # price_range.append(about_block.find('div', {'class':'hotels-hotel-review-about-addendum-AddendumItem__content--28NoV'}).text)
+        # star
+        # try:
+        #     hotel_star.append(soup.find('div', {'class':'starRating detailListItem'}).text)
+        # except AttributeError:
+        #     hotel_star.append(None)
 
-    # star
-    # try:
-    #     hotel_star.append(soup.find('div', {'class':'starRating detailListItem'}).text)
-    # except AttributeError:
-    #     hotel_star.append(None)
-
-    # rating_chart
-    rating_chart = soup.find('div', {'class':"choices"})
-    p_Excellent.append(rating_chart.find_all('div', {'class':'ui_checkbox item'})[0].text)
-    p_VeryGood.append(rating_chart.find_all('div', {'class':'ui_checkbox item'})[1].text)
-    p_Average.append(rating_chart.find_all('div', {'class':'ui_checkbox item'})[2].text)
-    p_Poor.append(rating_chart.find_all('div', {'class':'ui_checkbox item'})[3].text)
-    p_Terrible.append(rating_chart.find_all('div', {'class':'ui_checkbox item'})[4].text)
-    # gmap_src = soup.find('img').get("src")
-    # try:
-    #     driver.find_element_by_css_selector('#taplc_resp_hr_atf_hotel_info_0 > div > div.ui_column.is-12-tablet.is-2-mobile.hotelActionsColumn > div > div > div > div.is-hidden-mobile.blEntry.website.ui_link > span.blue_test.detail').click()
-    #     driver.switch_to_window(driver.window_handles[1])
-    #     hotel_url = driver.current_url
-    #     driver.close()
-    #     driver.switch_to_window(driver.window_handles[0])
-    # except:
-    #     hotel_url = None
-    time.sleep(8)
-
-df = pd.DataFrame({
-    'hotel_name':hotel_name , 'hotel_url':hotel_url, 'address':address, 
-    'n_comment':n_comment,'rank':rank, 'phone':phone,# 'comment_star':comment_star,
-    'p_Excellent':p_Excellent, 'p_VeryGood':p_VeryGood, 'p_Average':p_Average,
-    'p_Poor':p_Poor, 'p_Terrible':p_Terrible
-    # 'price_range':price_range
-    # 'hotel_star':hotel_star
-                })
-
-df.to_csv('content_parser.csv', index=False, encoding='utf-8')
+        # rating_chart
+        rating_chart = soup.find('div', {'class':"choices"})
+        p_Excellent = rating_chart.find_all('div', {'class':'ui_checkbox item'})[0].text
+        p_VeryGood = rating_chart.find_all('div', {'class':'ui_checkbox item'})[1].text
+        p_Average = rating_chart.find_all('div', {'class':'ui_checkbox item'})[2].text
+        p_Poor = rating_chart.find_all('div', {'class':'ui_checkbox item'})[3].text
+        p_Terrible = rating_chart.find_all('div', {'class':'ui_checkbox item'})[4].text
+        # try:
+        #     driver.find_element_by_css_selector('#taplc_resp_hr_atf_hotel_info_0 > div > div.ui_column.is-12-tablet.is-2-mobile.hotelActionsColumn > div > div > div > div.is-hidden-mobile.blEntry.website.ui_link > span.blue_test.detail').click()
+        #     driver.switch_to_window(driver.window_handles[1])
+        #     hotel_url = driver.current_url
+        #     driver.close()
+        #     driver.switch_to_window(driver.window_handles[0])
+        # except:
+        #     hotel_url = None
+        writer.writerow(
+                         {
+                            'hotel_id':hotel_id,
+                            'address':address,
+                            'rank':rank,
+                            'phone':phone,
+                            'p_Excellent':p_Excellent, 
+                            'p_VeryGood':p_VeryGood, 
+                            'p_Average':p_Average,
+                            'p_Poor':p_Poor, 
+                            'p_Terrible':p_Terrible
+                         }
+                        )
+        time.sleep(8)
--- url_parser.py
+++ url_parser.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 import requests
 import time
 import csv
+import re
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
@@ -17,12 +18,6 @@ driver.maximize_window()
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 domain = 'https://www.tripadvisor.com.tw'
 
-# columns
-# hotel_name = []
-# url = []
-# n_comment = []
-# rank_in_country = []
-
 # scrape page
 next_page = '//*[@id="taplc_main_pagination_bar_dusty_hotels_resp_0"]/div/div/div/span[2]'
 check_last_page = '#taplc_main_pagination_bar_dusty_hotels_resp_0 > div > div > div > div > span.pageNum.last.taLnk'
@@ -31,26 +26,32 @@ page_list = range(int(soup.select(check_last_page)[0].get('data-page-number')))
 print("Total number of page: {}".format(len(page_list)))
 
 with open('./data/url_parser.csv', 'a') as csvfile:
-    fieldnames = ['hotel_name', 'n_comment', 'rank_in_country', 'url']
+    fieldnames = ['hotel_id', 'hotel_name', 'n_comment', 'rank_in_country', 'url']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
+    index = 0
 
     for p in page_list:
         print('the number of page = {0}/{1}'.format(p+1, len(page_list)))
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        for element in soup.find_all('div', {"class": "prw_rup prw_meta_hsx_responsive_listing ui_section listItem"}):
-            # hotel_name.append(element.find('div', {"class": "listing_title"}).text)
-            # url.append(domain+element.find('div', {"class": "listing_title"}).find('a').get('href'))
-            # n_comment.append(element.find('a', {"class": "review_count"}).text)
-            # rank_in_country.append(element.find('div', {"class": "popindex"}).text)
+        hotel_blocks = soup.find_all('div', {"class": "prw_rup prw_meta_hsx_responsive_listing ui_section listItem"})
+
+        for element in hotel_blocks:
+            index += 1
             hotel_name = element.find('div', {"class": "listing_title"}).text
             url = domain+element.find('div', {"class": "listing_title"}).find('a').get('href')
             n_comment = element.find('a', {"class": "review_count"}).text
+            n_comment = re.sub('[^0-9,]', "", n_comment).replace(',','')
             rank_in_country = element.find('div', {"class": "popindex"}).text
-            writer.writerow({'hotel_name':hotel_name.encode("utf-8"),
-                             'n_comment':n_comment.encode("utf-8"),
-                             'rank_in_country':rank_in_country.encode("utf-8"),
-                              'url':url})
+            writer.writerow(
+                            {
+                                'hotel_id':index,
+                                'hotel_name':hotel_name.encode("utf-8"),
+                                'n_comment':n_comment,
+                                'rank_in_country':rank_in_country.encode("utf-8"),
+                                'url':url
+                            }
+                           )
         try:
             driver.execute_script(page_down)
             time.sleep(5)
@@ -58,10 +59,5 @@ with open('./data/url_parser.csv', 'a') as csvfile:
             time.sleep(8)
         except:
             print('in the end')
-
-# df = pd.DataFrame({'hotel_name':hotel_name, 'url':url,
-#                    'n_comment':n_comment, 'rank_in_country':rank_in_country })
-
-# df.to_csv('url_parser.csv', index=False, encoding='utf-8')
 
 driver.quit()
