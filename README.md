a9994dadd6f1016f8c3751805e2a38fa1855edee
Li-Jen Tang <b00701160@gmail.com>
2019-01-20 04:58:25 +0800

    refine code

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Tripadvisor_crawler
-This project can help you scrape hotel information from Tripadvisor. I divide the process into three steps as following:
+This project can help you scrape hotel information from Tripadvisor. I divide the process into two steps as following:
 ## Step1
  In **url_parser.py**, scrape and save **url_parser.csv** on data folder  
  (including hotels' basic information ex. hotal name, url, number of comments, hotel rank in country, etc.) 
 ## Step2
- In **content_parser.py**, scrape hotels' detailed information baesd on Step1 output ex. hotel website, star, price, latlon, etc.
-## Step3
- In **data_manipulation.py**, do feature engineering or regular expression to extact content on Step2 output.
+ In **content_parser.py**, scrape hotels' detailed information baesd on Step1 output ex. hotel rank, phone number, numbers of comments in each rank, etc.
+<!-- ## Step3
+ In **data_manipulation.py**, do feature engineering or regular expression to extact content on Step2 output. -->
 
 ## Preparation 
 1. Python2
 2. Use Chromedriver or PhantomJS
 3. Set your target_url in url_parser.py
+4. Turn off debug mode in content_parser.py
  
 ## To Be Continued
 If you have any feature requests, don't hesitate to contact me :)
--- content_parser.py
+++ content_parser.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import csv
 import numpy as np
@@ -5,24 +6,29 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
 import time
+import re
 
 domain = 'https://www.tripadvisor.com.tw'
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
-
 df = pd.read_csv('./data/url_parser.csv')
 total_hotels = len(df)
+debug = True
+if debug:
+    limit = 10
+else:
+    limit = None
+
 with open('./data/content_parser.csv', 'a') as csvfile:
     fieldnames = [
-                    'hotel_id', 'address', 'rank', 
-                    'phone','p_Excellent', 'p_VeryGood':, 'p_Average',
-                    'p_Poor', 'p_Terrible'
+                    'hotel_id', 'address', 'rank', 'phone',
+                    'n_Excellent', 'n_VeryGood', 'n_Average', 'n_Poor', 'n_Terrible'
                  ]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
-    for i, u in df[['hotel_id','url']]:
-        print('process = {}/{}'.format(i+1, total_hotels))
-        r = requests.get(u, headers=headers)
+    for index, u in enumerate(df['url'][:limit]):
+        hotel_id = df['hotel_id'][index]
+        print('process = {}/{}'.format(index+1, total_hotels))
+        r = requests.get(u)
         soup = BeautifulSoup(r.text, 'html.parser')
         # info_block
         info_block = soup.find('div', {'id':"atf_header"})
@@ -32,44 +38,31 @@ with open('./data/content_parser.csv', 'a') as csvfile:
         except AttributeError:
             phone = None
         rank = info_block.find('span', {'class':"header_popularity popIndexValidation"}).text
-        # comment_star.append(soup.find('span', {'class':'hotels-hotel-review-about-with-photos-Reviews__overallRating--3cjYf'}).text)
 
         # about_block
         about_block = soup.find('div', {'class':"hotels-hotel-review-layout-Section__plain--1HV0a"})
-        # price_range.append(about_block.find('div', {'class':'hotels-hotel-review-about-addendum-AddendumItem__content--28NoV'}).text)
-
-        # star
-        # try:
-        #     hotel_star.append(soup.find('div', {'class':'starRating detailListItem'}).text)
-        # except AttributeError:
-        #     hotel_star.append(None)
 
         # rating_chart
         rating_chart = soup.find('div', {'class':"choices"})
-        p_Excellent = rating_chart.find_all('div', {'class':'ui_checkbox item'})[0].text
-        p_VeryGood = rating_chart.find_all('div', {'class':'ui_checkbox item'})[1].text
-        p_Average = rating_chart.find_all('div', {'class':'ui_checkbox item'})[2].text
-        p_Poor = rating_chart.find_all('div', {'class':'ui_checkbox item'})[3].text
-        p_Terrible = rating_chart.find_all('div', {'class':'ui_checkbox item'})[4].text
-        # try:
-        #     driver.find_element_by_css_selector('#taplc_resp_hr_atf_hotel_info_0 > div > div.ui_column.is-12-tablet.is-2-mobile.hotelActionsColumn > div > div > div > div.is-hidden-mobile.blEntry.website.ui_link > span.blue_test.detail').click()
-        #     driver.switch_to_window(driver.window_handles[1])
-        #     hotel_url = driver.current_url
-        #     driver.close()
-        #     driver.switch_to_window(driver.window_handles[0])
-        # except:
-        #     hotel_url = None
+        rating_dict = {'Excellent':None,'VeryGood':None,'Average':None,'Poor':None,'Terrible':None}
+        for i, col in enumerate(rating_dict):
+            try:
+                tmp = rating_chart.find_all('div', {'class':'ui_checkbox item'})[i].text
+                tmp = int(re.sub('[^0-9,]', "", tmp).replace(',',''))
+                rating_dict[col] = tmp
+            except:
+                rating_dict[col] = None
         writer.writerow(
                          {
                             'hotel_id':hotel_id,
                             'address':address,
-                            'rank':rank,
+                            'rank':rank.encode('utf-8'),
                             'phone':phone,
-                            'p_Excellent':p_Excellent, 
-                            'p_VeryGood':p_VeryGood, 
-                            'p_Average':p_Average,
-                            'p_Poor':p_Poor, 
-                            'p_Terrible':p_Terrible
+                            'n_Excellent':rating_dict["Excellent"], 
+                            'n_VeryGood':rating_dict["VeryGood"], 
+                            'n_Average':rating_dict["Average"],
+                            'n_Poor':rating_dict["Poor"],
+                            'n_Terrible':rating_dict["Terrible"]
                          }
                         )
-        time.sleep(8)
+        time.sleep(10)
