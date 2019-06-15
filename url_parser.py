ea177da4e231edaaf35bcf71786c66e55a70c29d
Tang-Li-Jen <b00701160@gmail.com>
2019-06-16 02:08:15 +0800

    add check num of property type, select all property, scrape the main source and its price

--- url_parser.py
+++ url_parser.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
 import requests
 import time
 import csv
@@ -9,18 +10,33 @@ import re
 
 options = webdriver.ChromeOptions()
 # options.add_argument('headless')
-target_url = 'https://www.tripadvisor.com.tw/Hotels-g294225-Indonesia-Hotels.html'
+target_url = 'https://www.tripadvisor.com.tw/Hotels-g293808-Madagascar-Hotels.html'
 driver = webdriver.Chrome('./chromedriver', chrome_options=options)
 driver.get(target_url)
-# driver.maximize_window()
+driver.maximize_window()
 
 # Property Type expand all
-expand_all = driver.find_element_by_xpath("""//*[@id="component_7"]/div/div[2]/div[3]/div[2]/div[7]/span""")
-expand_all.click()
+try:
+    expand_all = driver.find_element_by_xpath("""//*[@id="component_6"]/div/div[2]/div[3]/div[2]/div[7]/span""")
+    expand_all.click()
+    component_id = 'component_6'
+except:
+    expand_all = driver.find_element_by_xpath("""//*[@id="component_7"]/div/div[2]/div[3]/div[2]/div[7]/span""")
+    expand_all.click()
+    component_id = 'component_7'
 
-for i in range(2, 13):
-    checkbox = driver.find_element_by_xpath('//*[@id="component_7"]/div/div[2]/div[3]/div[2]/div[%i]/div/label/div/span[1]/a/span' %i)
+# select_checkbox_8 = driver.find_element_by_xpath('//*[@id="checkbox_8"]')
+# driver.execute_script("arguments[0].click();", select_checkbox_8)
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+tmp = soup.find_all('div', {"class": "common-filters-FilterWrapper__content--3RxLJ"})
+num_of_property_type = len(tmp[2].find_all('div', {'class':'common-filters-CheckboxList__checkboxWrapper--3_ghM'}))
+
+for i in range(2, num_of_property_type+1):
+    checkbox = driver.find_element_by_xpath('//*[@id="{0}"]/div/div[2]/div[3]/div[2]/div[{1}]/div/label/div/span[1]/a/span'.format(component_id, i))
     checkbox.click()
+
+time.sleep(20)
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 domain = 'https://www.tripadvisor.com.tw'
@@ -33,7 +49,7 @@ page_list = range(int(soup.select(check_last_page)[0].get('data-page-number')))
 print("Total number of page: {}".format(len(page_list)))
 
 with open('./data/url_parser.csv', 'a') as csvfile:
-    fieldnames = ['hotel_id', 'hotel_name', 'n_comment', 'rank_in_country', 'url']
+    fieldnames = ['hotel_id', 'hotel_name', 'main_source', 'main_price', 'n_comment', 'rank_in_country', 'url']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     index = 0
@@ -50,10 +66,17 @@ with open('./data/url_parser.csv', 'a') as csvfile:
             n_comment = element.find('a', {"class": "review_count"}).text
             n_comment = re.sub('[^0-9,]', "", n_comment).replace(',','')
             rank_in_country = element.find('div', {"class": "popindex"}).text
+
+            price_and_source = element.find('div', {'class':'priceBlock ui_column is-12-tablet'})
+            main_price = price_and_source.find('div', {'class':'price __resizeWatch'}).text
+            main_source = price_and_source.find('span', {'class':'provider_text'}).text
+
             writer.writerow(
                             {
                                 'hotel_id':index,
                                 'hotel_name':hotel_name.encode("utf-8"),
+                                'main_source':main_source.encode("utf-8"),
+                                'main_price':main_price,
                                 'n_comment':n_comment,
                                 'rank_in_country':rank_in_country.encode("utf-8"),
                                 'url':url
@@ -61,9 +84,9 @@ with open('./data/url_parser.csv', 'a') as csvfile:
                            )
         try:
             driver.execute_script(page_down)
-            time.sleep(10)
+            time.sleep(15)
             driver.find_element_by_xpath(next_page).click()
-            time.sleep(10)
+            time.sleep(15)
         except:
             print('in the end')
 
